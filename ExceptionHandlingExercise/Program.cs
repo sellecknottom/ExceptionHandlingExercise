09134ba6726dbddbc2d066426e5c4f3f3ed910fa
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-21 13:48:17 -0600

    char to string to int

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -20,17 +20,33 @@ namespace ExceptionHandlingExercise
             //TODO START HERE:
 
             // 1) Create an char[], it must contain 6 numbers and 3 letters - name it arr
+            char[] arr = { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
             
             // 2) Create a list called numbers that will hold integers
-        
+            List<int> numbers = new List<int>();
+
             // 3) Create an string variable with an empty string initializer - name it str
-            
+            string str = "";
+
             // 4) Make a foreach loop to iterate through your character array            
-                // 5) Create a try-catch inide of your foreach loop
-                    // 6) Inside the try block: 
-                        // 7) Set your string variable to each array element in your char[] to .ToString()
-                        // 8) Now, using int.Parse, parse your string variable and store in an int variable
-                        // 9) Then add each int to your list
+            // 5) Create a try-catch inide of your foreach loop
+            // 6) Inside the try block: 
+            // 7) Set your string variable to each array element in your char[] to .ToString()
+            // 8) Now, using int.Parse, parse your string variable and store in an int variable
+            // 9) Then add each int to your list
+            foreach (char character in arr)
+            {
+                try
+                {
+                    str = character.ToString();
+                    int number = int.Parse(str);
+                    numbers.Add(number);
+                }
+                catch 
+                {
+                    Console.WriteLine($"Unable to Parse {character}");
+                }
+            }
                 
                    // 10) Inside the catch block:
                        // 11) In the scope of your catch you can use the following code:                  
@@ -38,10 +54,10 @@ namespace ExceptionHandlingExercise
                 
             
             // Uncomment the code below to see the numbers you successfully added to the numbers list: 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
